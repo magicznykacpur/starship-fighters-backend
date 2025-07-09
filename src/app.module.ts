cef1ce325e03d5f3e636f14838b9b61b6245c857
Kacper Zelichowski <kacper.zelichowski93@gmail.com>
2025-07-09 13:06:17 +0200

    feat: update gitignore, add graphql packages, remove redundant generated code, add a docker-compose with db service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      ...(process.env.NODE_ENV === 'PRODUCTION' && { graphiql: false }),
+    }),
+  ],
   controllers: [],
-  providers: [],
 })
 export class AppModule {}
