8b5816a4e15f4b996ffc9f66063e41a5c73bb565
Kacper Zelichowski <kacper.zelichowski93@gmail.com>
2025-07-09 14:54:24 +0200

    feat: add configmodule, remove prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { StarshipsModule } from './starships/starships.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      ...(process.env.NODE_ENV === 'PRODUCTION' && { graphiql: false }),
+      ...(process.env.NODE_ENV === 'production' && { graphiql: false }),
     }),
+    StarshipsModule,
   ],
-  controllers: [],
 })
 export class AppModule {}
